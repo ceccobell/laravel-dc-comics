6dc9975f614331b3336f97e9eba94ca44b0e6f3d
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-10-01 16:41:50 +0200

    [UPD] database data printed on the screen

---
+++ config/exploreDC.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    [
+        'name' => 'DIGITAL COMICS',
+        'img' => 'resources/images/buy-comics-digital-comics.png',
+    ],
+    [
+        'name' => 'DC MERCHANDISE',
+        'img' => 'resources/images/buy-comics-merchandise.png',
+    ],
+    [
+        'name' => 'SUBSCRIPTION',
+        'img' => 'resources/images/buy-comics-subscriptions.png',
+    ],
+    [
+        'name' => 'COMIC SHOP LOCATOR',
+        'img' => 'resources/images/buy-comics-shop-locator.png',
+    ],
+    [
+        'name' => 'DC POWER VISA',
+        'img' => 'resources/images/buy-dc-power-visa.svg',
+    ]
+];
---
+++ resources/views/comics.blade.php
@@ -0,0 +1,41 @@
+@extends('app')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 h-300 position-relative">
+                <img src="{{ Vite::asset('resources/images/jumbotron.jpg')}}" alt="" class="jumbotron">
+                <div class="content">
+                    <span class="text-white py-2 px-3 bg-blue p-abs fw-semibold">CURRENT SERIES</span>
+                </div>
+            </div>
+            <div class="row bg-black">
+                <div class="content d-flex flex-wrap py-3"> 
+                    @foreach ($comics as $comic)
+                        <div class="col-2 p-3">
+                            <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}" class="img-card">
+                            <h6 class="text-white text-uppercase mt-2">{{ $comic['series'] }}</h6>
+                        </div>
+                    @endforeach
+                    <div class="col-12 text-center mb-2">
+                        <span class="text-white py-2 px-5 bg-blue">LOAD MORE</span>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 bg-blue">
+                    <div class="content justify-content-center py-4">
+                        <ul class="d-flex align-items-center list-unstyled m-0 p-0">
+                            @foreach ($exploreDC as $item)
+                            <li class="mx-3 d-flex align-items-center">
+                                <img src="{{ Vite::asset($item['img'])}}" alt="" class="img-fluid icon me-2">
+                                <a href="#" class="text-decoration-none text-white">{{ $item['name'] }}</a>
+                            </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
